a97b596dfd59af1f16784566a5866af9e0689e64
VITOR SILVA CARVALHO <vitor41197@gmail.com>
2025-03-31 20:27:43 -0300

    criado classe Circuito

---
+++ HotLapping/Models/Circuito.cs
@@ -0,0 +1,19 @@
+﻿namespace HotLapping.Models
+{
+    public class Circuito
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Pais { get; set; }
+        public string Cidade { get; set; }
+        public string Tamanho { get; set; }
+        public string NumeroCurvas { get; set; }
+        public string Recorde { get; set; }
+        public string RecordePiloto { get; set; }
+        public string RecordeCarro { get; set; }
+        public string Tipo { get; set; }
+        public string Imagem { get; set; }
+
+        public Circuito()
+    }
+}
--- HotLapping/Program.cs
+++ HotLapping/Program.cs
@@ -24,4 +24,8 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapControllerRoute(
+    name:"circuito",
+    pattern: "{controller=Circuito}/{action=Index}/{id?}");
+
 app.Run();
